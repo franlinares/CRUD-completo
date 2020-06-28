e5aafff6a8e8b1982587870545028a91d6c0fbcc
Fran <franlinaresgorria@gmail.com>
2020-06-28 20:00:55 +0200

    Last part of the proyect, editing, is done

--- routes/users.js
+++ routes/users.js
@@ -79,7 +79,7 @@ router.get('/eliminar/:user_id', function(req, res) {
 })
 
 
-//GET users Editar para traernos los datos del usuario en concreto ----- EDIT----------
+// Get users/perfil para ver el perfil individual y para iniciar la edicion
 
 router.get('/perfil/:user_id', function(req, res){
   console.log(req.params)
@@ -91,6 +91,40 @@ router.get('/perfil/:user_id', function(req, res){
     res.render('userProfile', {result})
   })
   
+})
+
+//GET y POST users Editar  ----- EDIT----------
+
+router.get('/edit/:user_id', function(req, res){
+  console.log(req.params)
+  let user_id = req.params.user_id;
+  let sql = `SELECT * FROM user, phone WHERE user.phone_id = phone.phone_id AND user_id = ${user_id}`;
+
+  connection.query(sql, function(err, result){
+    if(err)throw err;
+    res.render('edit', {result});
+  })
+
+})
+
+router.post('/update/:user_id', function(req, res) {
+  console.log(req.params);
+  let user_id = req.params.user_id;
+  console.log(req.body);
+  let name = req.body.name;
+  let last_name = req.body.last_name;
+  let model = req.body.model;
+  let color = req.body.color;
+  // let name = req.body.name;
+  let sql = `UPDATE  user SET name = '${name}', last_name = '${last_name}' WHERE user_id= ${user_id}`;
+  connection.query(sql, function(err, result) {
+    if (err) throw err;
+  })
+  let sqlPhone = `UPDATE phone SET model = '${model}', color = '${color}' WHERE phone_id = (SELECT phone_id FROM user WHERE user_id = ${user_id}) `
+  connection.query(sqlPhone, function(err, results){
+    if (err) throw err;
+    res.redirect('/users/Listado')
+  })
 })
 
 module.exports = router;
